1d3178e757eb8dd3cb0ee5b3f3572bde105e107f
Torben Jepsen <torbenjepsen@outlook.com>
2018-04-17 13:29:27 -0500

    added server functions

--- public/script.js
+++ public/script.js
@@ -1,5 +1,9 @@
 console.log('js is ready');
 let totalMonthly = 0
+// variable declared for updating monthly expenses
+const employeeList = [];
+// const for creating employee class and array
+
 
 $(document).ready(onReady);
 
@@ -7,6 +11,7 @@ function onReady() {
     console.log('jquery is ready');
     $('#addEmployeeButton').on('click', clickHandler);
     updateTotalSalary();
+    $('#employeeList').on('click', '.btn-delete', deleteEmployee);
 }
 
 function clickHandler() {
@@ -15,7 +20,24 @@ function clickHandler() {
     addNewEmployee();
 }
 
+function Employee (first, last, id, title, salary) {
+    this.firstName = first;
+    this.lastName = last;
+    this.employeeId = id;
+    this.title = title;
+    this.annualSalary = parseInt(salary);
+}
+// above for employee class and array
+
 function addNewEmployee() {
+    const employee = new Employee(
+        $('#firstNameInput').val(),
+        $('#lastNameInput').val(),
+        $('#idInput').val(),
+        $('#titleInput').val(),
+        $('#salaryInput').val()
+    );
+    // above for employee class and array
     $('#employeeList').append(
         '<tr>' +  
             '<td>' + $('#firstNameInput').val() + '</td>' +  
@@ -23,13 +45,18 @@ function addNewEmployee() {
             '<td>' + $('#idInput').val() + '</td>' +
             '<td>' + $('#titleInput').val() + '</td>' +
             '<td>' + $('#salaryInput').val() + '</td>' +
+            '<td>' + '<button class="btn-delete">Delete</button>' + '</td>' +
         '</tr>'
     );
+    // for clearing input fields I could've called the html tag and done: $('input').val('');
     $('#firstNameInput').val('');
     $('#lastNameInput').val('');
     $('#idInput').val('');
     $('#titleInput').val('');
     $('#salaryInput').val('');
+    // below for employee class and array
+    employeeList.push(employee);
+
 }
 
 function updateTotalSalary() {
@@ -42,4 +69,10 @@ function updateTotalSalary() {
         $('#monthlyCost').css('color', 'red');
     }
     
+}
+
+function deleteEmployee() {
+    const rowToDelete = $(this).closest('tr');
+    rowToDelete.remove();
+
 }
